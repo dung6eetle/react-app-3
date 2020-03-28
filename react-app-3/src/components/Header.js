1b1d11665abd3aca9a98905ebd718d940cf8bd19
bug-dor <tolyaumer@rambler.ru>
2020-03-28 13:18:02 +0300

    added Header,Footer components

---
+++ react-app-3/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+            <h1>Hi MaN</h1>
+              <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+              </ul>
+          </nav>
+        </header>
+    )
+}
+export default Header
---
+++ react-app-3/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Puppidup</h1>
+        <p>like banana</p>
+        <ul>
+          <li>Jerasalim</li>
+          <li>Tokio</li>
+          <li>Barselona</li>
+        </ul>
+      </div>
+    )
+  }
+
+  export default MyInfo
