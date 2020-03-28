07ab894e1227fdcf8aa77d7bdf69b6d4de8fe9a4
bug-dor <tolyaumer@rambler.ru>
2020-03-28 17:53:43 +0300

    added style to timeOfDay

--- react-app-3/src/components/Header.js
+++ react-app-3/src/components/Header.js
@@ -5,19 +5,24 @@ function Header() {
     const date = new Date()
     const hours = date.getHours()
     let timeOfDay
+    const styles ={}
+    
 
     if (hours < 12) {
         timeOfDay = "morning"
+        styles.color = "#FFFA5A"
     } else if (hours >= 12 && hours <17) {
         timeOfDay = "afternoon"
+        styles.color = "#F08D1D"
     } else {
         timeOfDay = 'night'
+        styles.color = "#EE5306"
     }
 
     return (
         <header className='navbar'>
-           <h1>TODO BUG-DOR</h1>
-           <p>Good {timeOfDay}!</p>
+           <h1>TODO by BUG-DOR</h1>
+           <p style={styles}>Good {timeOfDay}!</p>
         </header>
     )
 }
