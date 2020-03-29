a4b8631e3044533e7fb8b6761df74e1e592656de
bug-dor <tolyaumer@rambler.ru>
2020-03-29 14:51:43 +0300

    pause in work TODO

--- react-app-3/src/components/MainContent.js
+++ react-app-3/src/components/MainContent.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import './MainContent.css'
+import TodoItem from './TodoItem'
 
 function MainContent() {
     return (
-        <div className="todo">
-            <nav>
-            <ul>
-                <li>eating</li>
-                <li>sleeping</li>
-                <li>working</li>
-              </ul>
-            </nav>
+        <div className="todo-list">
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
         </div>
     )
 }
