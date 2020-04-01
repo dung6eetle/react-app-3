c8bce6de2134c4e9beeb3af46bc94a4ec8e4a58b
bug-dor <tolyaumer@rambler.ru>
2020-04-01 16:22:31 +0300

    added TodoItem=> input =>onChange{Click}

--- react-app-3/src/components/TodoItem.js
+++ react-app-3/src/components/TodoItem.js
@@ -3,9 +3,10 @@ import '../index.css'
 import todoData from './todoData'
 
 function TodoItem(props) {
+    
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed} />
+            <input type="checkbox" checked={props.item.completed} onChange={() => console.log('CLICK!')} />
             <p>{props.item.text}</p>
         </div>
     )
